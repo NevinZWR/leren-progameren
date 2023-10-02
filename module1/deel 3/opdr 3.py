5fb6b6307afeb46f87830f061aac949c0759627c
NevinZW <nevin.zweere@gmail.com>
2023-10-02 11:48:56 +0200

    else-statement

---
+++ module1/deel 3/opdr 3.py
@@ -0,0 +1,11 @@
+a = int(input("Voer het eerste gehele getal in: "))
+b = int(input("Voer het tweede gehele getal in: "))
+
+if a > b:
+    Max = a
+    print("a is het grootste getal:", Max)
+elif a < b:
+    Min = a
+    print("a is het kleinste getal:", Min)
+else:
+    print("a en b zijn gelijk aan elkaar.")
